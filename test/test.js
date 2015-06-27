d70cdfdc98783a1d6d443819c0d4c61bd3b7d2d7
Marc Tompkins <marc.tompkins@viasat.com>
2015-06-26 17:41:59 -0700

    added real tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,28 @@
-var assert = require("assert")
-describe('Array', function() {
-   describe('#indexOf()', function() {
-      it('should return -1 when the value is not present', function() {
-         assert.equal(-1, [1,2,3].indexOf(5));
-	 assert.equal(-1, [1,2,3].indexOf(0));
-      })
-   })
-})
+#!/usr/bin/env node
+'use string';
+
+var assert = require("assert");
+var restify = require('restify');
+
+var client = restify.createJsonClient({
+   url: 'http://localhost:3000'
+});
+
+
+describe('endpoint /autos', function() {
+   //describe('test GET /autos', function() {
+      it('should return 200 for GET', function(done) {
+         client.get('/autos', function(err, req, res, autos) {
+            assert.ifError(err);
+            assert.equal(200, res.statusCode, 'status code not 200');
+            done();
+         });
+      });
+      it('should return 405 for DELETE', function(done) {
+         client.del('/autos', function(err, req, res, obj) {
+            assert.equal(405, res.statusCode, 'status code not 405');
+            done();
+         });
+      });
+   //});
+});
